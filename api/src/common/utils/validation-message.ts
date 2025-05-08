4a0eff6a17406d15e0cd2f23f3e56b6a2ba2e402
gsousa12 <wgsousa12@gmail.com>
2025-05-08 17:07:50 -0300

    feat(api): tax id field

--- api/src/common/utils/validation-message.ts
+++ api/src/common/utils/validation-message.ts
@@ -20,5 +20,5 @@ export const validationMessages = {
 
   isDefined: (args: ValidationArguments) =>
     `O campo ${args.property} é obrigatório e deve está presente no payload de request.`,
-  taxID: (args: ValidationArguments) => `O campo ${args.property} deve ser um CNPJ ou CPF válido`,
+  taxId: (args: ValidationArguments) => `O campo ${args.property} deve ser um CNPJ ou CPF válido`,
 };
